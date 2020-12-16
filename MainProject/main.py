96ab12fc97ecfb416840bd9d7c5b04ae25277228
Benjamin <Bendotlami@gmail.com>
2020-12-16 10:45:40 +0200

    Added MainProject folder + started implementation

---
+++ MainProject/main.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+from stn import *
+
+
+def train(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 500 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                       100. * batch_idx / len(train_loader), loss.item()))
+
+
+if __name__ == "__main__":
+    # plt.ion()   # interactive mode
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model = GeneratorNet().to(device)
+    optimizer = optim.SGD(model.parameters(), lr=0.01)
---
+++ MainProject/stn.py
@@ -0,0 +1,83 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class GeneratorNet(nn.Module):
+    def __init__(self):
+        super(GeneratorNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+        # Spatial transformer localization-network
+        self.localization = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=7),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True),
+            nn.Conv2d(8, 10, kernel_size=5),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True)
+        )
+
+        # Regressor for the 3 * 2 affine matrix
+        self.fc_loc = nn.Sequential(
+            nn.Linear(10 * 3 * 3, 32),
+            nn.ReLU(True),
+            nn.Linear(32, 3 * 2)
+        )
+
+        # Initialize the weights/bias with identity transformation
+        self.fc_loc[2].weight.data.zero_()
+        self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+
+    # Spatial transformer network forward function
+    def stn(self, x):
+        xs = self.localization(x)
+        xs = xs.view(-1, 10 * 3 * 3)
+        theta = self.fc_loc(xs)
+        theta = theta.view(-1, 2, 3)
+
+        grid = F.affine_grid(theta, x.size())
+        x = F.grid_sample(x, grid)
+
+        return x
+
+    def forward(self, FG, BG):
+        # transform the FG input
+        FG_after_transform = self.stn(FG)
+
+        # concat FG to BG # TODO: fix this sum
+        concat_img = BG + FG_after_transform
+
+        return FG_after_transform
+
+
+class DiscriminatorNet(nn.Module):
+    def __init__(self, image_size=128, conv_dim=64, fc_dim=1024, n_layers=5):
+        super(DiscriminatorNet, self).__init__()
+        layers = []
+        in_channels = 3
+        for i in range(n_layers):
+            layers.append(nn.Sequential(
+                nn.Conv2d(in_channels, conv_dim * 2 ** i, 4, 2, 1),
+                nn.InstanceNorm2d(conv_dim * 2 ** i, affine=True, track_running_stats=True),
+                nn.LeakyReLU(negative_slope=0.2, inplace=True)
+            ))
+            in_channels = conv_dim * 2 ** i
+        self.conv = nn.Sequential(*layers)
+        feature_size = image_size // 2**n_layers
+        self.fc_adv = nn.Sequential(
+            nn.Linear(conv_dim * 2 ** (n_layers - 1) * feature_size ** 2, fc_dim),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.Linear(fc_dim, 1)
+        )
+
+    def forward(self, img):
+        # transform the FG input
+        y = self.conv(img)
+        y = y.view(y.size()[0], -1)
+        logit_adv = self.fc_adv(y)
+        return logit_adv
