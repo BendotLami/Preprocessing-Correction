64b125950496524c12e8e07cb5fbfbc28c15edf8
Benjamin <Bendotlami@gmail.com>
2021-02-09 14:31:55 +0200

    ColorCorrection brightness-contrast model

--- MainProject/ModelAgent2.py
+++ MainProject/ModelAgent2.py
@@ -10,13 +10,20 @@ import matplotlib.pyplot as plt
 from preprocess_model import *
 
 BATCH_SIZE = 64
+IMG_SIZE = 144
+
+
+def unexpand_vector(vector, tensor):
+    vector_unsqueezed = vector.unsqueeze(2).unsqueeze(3)
+    return vector_unsqueezed.expand_as(tensor)
+
 
 class ModelAgentColorCorrection(object):
     def __init__(self, dataset):
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-        self.model = ColorCorrectionNet().to(self.device)
-        self.optimizer = optim.Adam(self.model.parameters(), lr=0.01)
+        self.model = ColorCorrectionNet(IMG_SIZE).to(self.device)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=0.002)
         self.lr_scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=800000, gamma=0.1)
 
         self.dataset = dataset
@@ -26,6 +33,28 @@ class ModelAgentColorCorrection(object):
         )
         self.scripted_transforms = torch.jit.script(transforms)
 
+
+    def random_contrast_and_brightness(self, in_tensor):
+        batch_size = in_tensor.size()[0]
+        alpha = 0.7 + (1.5 - 0.7) * torch.rand(batch_size, 1)
+        beta = -0.5 + (0.5 + 0.5) * torch.rand(batch_size, 1)
+
+        alpha = alpha.to(self.device)
+        beta = beta.to(self.device)
+
+        out_tensor = in_tensor * unexpand_vector(alpha, in_tensor) + unexpand_vector(beta, in_tensor)
+
+        return out_tensor, torch.cat((1.0/alpha, -beta/alpha), 1)
+
+
+    def fix_image(self, in_image, alpha_beta):
+        alpha = alpha_beta[:, :1]
+        beta = alpha_beta[:, 1:]
+
+        out_image = in_image * unexpand_vector(alpha, in_image) + unexpand_vector(beta, in_image)
+
+        return out_image
+
     def train(self):
         train_size = int(0.9 * len(self.dataset))
         test_size = len(self.dataset) - train_size
@@ -34,16 +63,17 @@ class ModelAgentColorCorrection(object):
         train_data_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
         test_data_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
-        criterion = nn.L1Loss()
+        criterion = nn.MSELoss()
 
         for epoch in range(100):
             for batch_data in train_data_loader:
                 batch_data = batch_data.to(self.device)
-                data_augmented = self.scripted_transforms(batch_data).to(self.device)
+                # data_augmented = self.scripted_transforms(batch_data).to(self.device)
+                data_augmented, alpha_beta_fixed = self.random_contrast_and_brightness(batch_data)
 
-                img_reconstructed = self.model(data_augmented).to(self.device)
+                alpha_beta = self.model(data_augmented).to(self.device)
 
-                train_loss = criterion(img_reconstructed, batch_data)
+                train_loss = criterion(alpha_beta, alpha_beta_fixed)
 
                 self.optimizer.zero_grad()
                 train_loss.backward()
@@ -56,19 +86,20 @@ class ModelAgentColorCorrection(object):
                 save_idx = 0
                 for batch_data_test in test_data_loader:
                     batch_data_test = batch_data_test.to(self.device)
-                    data_augmented = self.scripted_transforms(batch_data_test).to(self.device)
+                    # data_augmented = self.scripted_transforms(batch_data_test).to(self.device)
+                    data_augmented, alpha_beta_fixed = self.random_contrast_and_brightness(batch_data_test)
 
-                    img_reconstructed = self.model(data_augmented).to(self.device)
+                    alpha_beta = self.model(data_augmented).to(self.device)
 
-                    print(criterion(img_reconstructed, batch_data_test).data)
+                    print(criterion(alpha_beta, alpha_beta_fixed).data)
 
                     test_examples_cpu = batch_data_test.cpu()
 
-                    reconstruction_cpu = img_reconstructed.cpu()
+                    reconstruction_cpu = self.fix_image(data_augmented, alpha_beta).cpu()
 
                     data_augmented = data_augmented.cpu()
 
-                    for index in range(int(len(img_reconstructed)/10)):
+                    for index in range(int(len(alpha_beta)/10)):
                         img = test_examples_cpu[index].numpy()
 
                         img_reconstruct = reconstruction_cpu[index].numpy()
@@ -76,6 +107,8 @@ class ModelAgentColorCorrection(object):
                         img_color_augmented = data_augmented[index].numpy()
 
                         valid_reconstruct_img = np.clip(img_reconstruct, 0, 1)
+
+                        img_color_augmented = np.clip(img_color_augmented, 0, 1)
 
                         plt.imsave(str("./test_output_color/" + str(epoch) + "_" + str(save_idx) + ".jpg"),
                                    np.concatenate((img.transpose(1, 2, 0), img_color_augmented.transpose(1, 2, 0),
--- MainProject/preprocess_model.py
+++ MainProject/preprocess_model.py
@@ -4,34 +4,43 @@ import torch.nn.functional as F
 
 
 class ColorCorrectionNet(nn.Module):
-    def __init__(self):
+
+    def create_layer_down(self, in_channels, out_channels):
+        rtn_object = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(negative_slope=0.1),
+            nn.MaxPool2d(kernel_size=(2, 2))
+        )
+        return rtn_object
+
+    def __init__(self, in_image_size):
         super(ColorCorrectionNet, self).__init__()
-        in_layers = 3
 
-        # geometric transform:
-        self.conv1 = nn.Sequential(nn.Conv2d(in_layers, in_layers, 4, 2, 1), nn.ReLU(True))
-        # in_layers += 3
-        # self.conv2 = nn.Sequential(nn.Conv2d(in_layers, in_layers, 4, 2, 1), nn.LeakyReLU(True))
-        # in_layers += 3
-        # self.conv3 = nn.Sequential(nn.Conv2d(in_layers, in_layers, 4, 2, 1), nn.LeakyReLU(True))
-        # in_layers += 3
-        # self.conv4 = nn.Sequential(nn.Conv2d(in_layers, in_layers, 5, 1, 2), nn.ReLU(True))
-        # in_layers += 3
-        # self.conv5 = nn.Sequential(nn.Conv2d(in_layers, in_layers, 4, 2, 2), nn.ReLU(True))
-        # in_layers += 3
+        self.conv1 = self.create_layer_down(3, 32)
+        self.conv2 = self.create_layer_down(32, 64)
+        self.conv3 = self.create_layer_down(64, 128)
+        self.conv4 = self.create_layer_down(128, 256)
+        self.conv5 = self.create_layer_down(256, 512)
 
-        self.linear_layer_1 = nn.Sequential(nn.Linear(in_layers * 72 * 72, 256), nn.ReLU(True))
-        self.linear_layer_2 = nn.Sequential(nn.Linear(256, 6))
+        img_size = int(in_image_size / 32)
+        self.linear_1 = nn.Sequential(nn.Linear(img_size * img_size * 512, 512),
+                                      nn.LeakyReLU(negative_slope=0.1))
+        self.linear_2 = nn.Sequential(nn.Linear(512, 64), nn.LeakyReLU(negative_slope=0.1))
+
+        self.linear_3 = nn.Linear(64, 2)
 
     def forward(self, image):
         feat = image
         feat = self.conv1(feat)
-        # feat = self.conv2(feat)
-        # feat = self.conv3(feat)
-        # feat = self.conv4(feat)
+        feat = self.conv2(feat)
+        feat = self.conv3(feat)
+        feat = self.conv4(feat)
+        feat = self.conv5(feat)
         feat = feat.view(feat.size()[0], -1)
-        feat = self.linear_layer_1(feat)
-        feat = self.linear_layer_2(feat)  # feat = [lambda_0, lambda_1, lambda_2, beta_0, beta_1, beta_2]
-        img = image
-        img = img * feat[:, :3, None, None] + feat[:, 3:, None, None]
-        return img
+        feat = self.linear_1(feat)
+        feat = self.linear_2(feat)
+        feat = self.linear_3(feat)  # [batch_size, 2]
+        # img = image
+        # img = img * feat[:, :3, None, None] + feat[:, 3:, None, None]
+        return feat
